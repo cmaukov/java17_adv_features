c6df2ff332f826a277c4325354442b4da4c32fc7
konstantinstaykov <k.staykov@gmail.com>
2022-07-31 22:45:33 -0400

    Creating Records with Builder

--- src/main/java/com/hop/Order.java
+++ src/main/java/com/hop/Order.java
@@ -5,6 +5,7 @@ package com.hop;
  */
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.hop.Validation.checkThat;
@@ -18,4 +19,38 @@ public record Order(long id, Customer customer, LocalDateTime dateTime, List<Ord
         lines = List.copyOf(lines);
     }
 
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder {
+        private long id;
+        private Customer customer;
+        private LocalDateTime dateTime;
+        public List<OrderLine> lines = new ArrayList<>();
+
+        public Builder withId(long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder forCustomer(Customer customer) {
+            this.customer = customer;
+            return this;
+        }
+
+        public Builder atDateTime(LocalDateTime dateTime) {
+            this.dateTime = dateTime;
+            return this;
+        }
+
+        public Builder addLine(OrderLine line) {
+            lines.add(line);
+            return this;
+        }
+
+        public Order build() {
+            return new Order(id, customer, dateTime, lines);
+        }
+    }
 }
