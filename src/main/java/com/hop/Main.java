e254190f265c8640e26e22ba408a4e3503921a99
konstantinstaykov <k.staykov@gmail.com>
2022-07-31 22:51:51 -0400

    using Builder

--- src/main/java/com/hop/Main.java
+++ src/main/java/com/hop/Main.java
@@ -4,8 +4,18 @@ package com.hop;
  * @author Konstantin Staykov
  */
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
+        var order = Order.builder()
+                .withId(123L)
+                .forCustomer(new Customer(1L, "Bob Jones", "bob@bob.com"))
+                .atDateTime(LocalDateTime.now())
+                .addLine(new OrderLine(new Product(232L, "Java Records", "Java records book"), 1, BigDecimal.valueOf(5.55)))
+                .build();
 
+        System.out.println(order);
     }
 }
